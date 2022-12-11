fc9d04655d3c8706986270df0b936b4556b97c8f
Konstantin <pkv90@yandex.ru>
2022-12-11 19:24:30 +1200

    Первое ДЗ по C#

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿//Задача 1. Написать программу, которая на ввод принимает 2 числа и выдаёт, какое число больше, а какое меньше
+
+/*
+Console.WriteLine ("Input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+    Console.WriteLine("Первое число больше, а второе меньше");
+else
+    Console.WriteLine("Второе число больше, а первое меньше");
+*/
+
+//Задача 2. Написать программу, которая на ввод принимает 3 числа и выдаёт максимальное из этих чисел.
+
+/*
+Console.WriteLine ("Введи первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи второе число: ");
+int num2 = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine ("Введи третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+*/
+
+//Задача 3. Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным.
+
+/*
+Console.WriteLine ("Введи число: ");
+int num = Convert.ToInt32 (Console.ReadLine());
+
+if (num%2 == 0)
+    Console.WriteLine("Число является чётным!");
+else
+    Console.WriteLine("Число нечётное!");
+*/
+
+//Задача 4. Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+
+/*
+Console.Write("Input an integer number: ");
+int  num = Convert.ToInt32(Console.ReadLine());
+
+int current = 1;
+
+while(current <= num)
+{
+    if (current%2 == 0)
+    Console.Write(current + " ");
+    current ++;
+    }
+*/
